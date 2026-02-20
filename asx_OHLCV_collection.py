af17d25861f813da3501dd8bf450d2d5604a3d7f
William Davis <william.2013999@gmail.com>
2026-02-20 18:16:54 +1000

    Changed OHLCV data format and added adjused close

--- asx_OHLCV_collection.py
+++ asx_OHLCV_collection.py
@@ -6,36 +6,56 @@ from datetime import date
 raw_names = get_names_from_etf()
 PATH = "asx_50_OHLCV_data.parquet"
 
+
+def reformat_OHLCV_df(df: pd.DataFrame) -> pd.DataFrame:
+    stacked_df = df.stack(level=0, future_stack=True).reset_index()
+    stacked_df.columns.name = None
+    stacked_df.columns = ["Date", "Ticker", "Open", "High", "Low", "Close", "Adj Close", "Volume"]
+    stacked_df["Date"] = pd.to_datetime(stacked_df["Date"])
+
+    return stacked_df
+
+
 def create_asx50_df(start_date = date.today().isoformat(), end_date = None) -> pd.DataFrame:
     raw_asx50_OHLCV_df = yf.download(
         tickers=raw_names,
-        start = start_date,
+        start=start_date,
         end=end_date,
-        group_by="ticker"
+        group_by="ticker",
+        auto_adjust=False
         )
+
+    if raw_asx50_OHLCV_df is None or raw_asx50_OHLCV_df.empty:
+        print("No new data returned. Most likely rate limited by yfinance")
+        quit()
+
 
     return raw_asx50_OHLCV_df
 
-def save_df_to_file(file_name: str, df: pd.DataFrame) -> None:
-    stacked_df = df.stack(level=0, future_stack=True).reset_index()
-    stacked_df.columns.name = None
-    stacked_df.columns = ['Date', 'Ticker', 'Open', 'High', 'Low', 'Close', 'Volume']
-    stacked_df['Date'] = pd.to_datetime(stacked_df['Date'])
-    file_exists = True
+def save_df_to_file(file_name: str) -> None:
+    df = open_OHLCV_data(file_name)
 
+    new_data = create_asx50_df(df["Date"].max().date())
+    new_data = reformat_OHLCV_df(new_data)
+
+    joined_data = pd.concat([df, new_data], ignore_index=True)
+
+    joined_data.drop_duplicates(subset=["Ticker", "Date"], keep="last", inplace=True, ignore_index=True)
+    joined_data.sort_values(["Ticker","Date"], inplace=True, ignore_index=True)
+    joined_data.dropna(subset=["Open","High","Low","Close"], inplace=True, ignore_index=True)
+
+    joined_data.to_parquet(path=file_name, engine="pyarrow", index=False)
+
+def open_OHLCV_data(file_name: str) -> pd.DataFrame:
     try:
-        curr_df = pd.read_parquet(path=file_name, engine="pyarrow")
+        df = pd.read_parquet(path=file_name, engine="pyarrow")
+        df["Date"] = pd.to_datetime(df["Date"])
+        return df
 
     except FileNotFoundError:
-        file_exists = False
+        df = create_asx50_df(start_date = "2000-01-01")
+        return reformat_OHLCV_df(df)
 
-    if file_exists:
-        stacked_df = pd.concat([curr_df, stacked_df], ignore_index=True, keep="last")
+#save_df_to_file(PATH)
 
-
-    stacked_df.drop_duplicates(inplace=True)
-    stacked_df.sort_values(['Ticker','Date'], inplace=True, ignore_index=True)
-    stacked_df.dropna(inplace=True, ignore_index=True)
-
-    stacked_df.to_parquet(path=file_name, engine="pyarrow", index=False)
-
+print(open_OHLCV_data(PATH))
