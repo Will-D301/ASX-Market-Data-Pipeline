5ad1959f1c7f60973233422fe2391de0297c152d
William Davis <william.2013999@gmail.com>
2026-03-01 02:09:38 +1000

    Refactored the repo structure to keep data out of src

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 ASX_50_ETF_EXCEL_LINK = "https://www.ssga.com/au/en_gb/intermediary/library-content/products/fund-data/etfs/apac/holdings-daily-au-en-sfy.xlsx"
 
-OHLCV_PATH = "./src/data_storage/ohlcv_data.parquet"
-FEATURE_PATH = "./src/data_storage/feature_data.parquet"
-DUCKDB_PATH = "./src/data_storage/market.db"
+OHLCV_PATH = "data/ohlcv_data.parquet"
+FEATURE_PATH = "data/feature_data.parquet"
+DUCKDB_PATH = "data/market.db"
